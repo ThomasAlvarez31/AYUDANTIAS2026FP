96377daa1fb1aaebe8fe6f29faceb638d51de580
Thomas31 <163472229+ThomasAlvarez31@users.noreply.github.com>
2026-06-10 08:26:18 -0400

    Add files via upload

---
+++ semana 3/usosdef.py
@@ -0,0 +1,43 @@
+#def nombre_de_la_funcion(parametros):
+    # Bloque de código que hace algo
+    #return resultado # (Opcional)
+'''
+def saludar():
+    print("¡Hola, programador! Bienvenido al menú.")
+
+# Para usarla, solo la llamamos:
+saludar()
+
+
+
+def sumar_dos_numeros(a, b):
+    resultado = a + b
+    print(f"La suma de {a} + {b} es: {resultado}")
+
+# Al usarla, le pasamos los datos que necesita
+sumar_dos_numeros(5, 7)
+
+
+#USO DE FOR DE MANERA PRACTIVA
+'''
+# 1. Tenemos nuestra lista de compras (el carrito)
+carrito = ["Manzana", "Leche", "Pan", "Cereal"]
+
+print("--- Iniciando el cobro ---")
+
+# 2. El ciclo 'for' revisa un producto a la vez
+for producto in carrito:
+    print(f"Escaneando: {producto} ... ¡Pip!")
+
+print("--- Compra terminada, gracias ---")
+
+'''
+#USO CON RANGE()
+print("--- ¡A entrenar! ---")
+
+# range(1, 6) le dice a Python que cuente desde el 1 hasta ANTES del 6
+for repeticion in range(1, 6):
+    print(f"Abdominal número {repeticion}... ¡Uff!")
+
+print("--- ¡Rutina terminada! Buen trabajo ---") 
+'''   
